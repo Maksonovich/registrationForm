a8e52a691df6c0c083fc5792ad0f8f868614adf2
Maksonovich <kaverron1@gmail.com>
2022-12-26 22:42:52 +0600

    Form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import useReducer from "./Store/Reducer/user";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import Users from './Pages/Users.jsx';
+import Home from './Pages/Home';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import "../src/App.css"
 
 function App() {
+  const store = createStore(useReducer)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path='/Users' element={<Users/>}/>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
